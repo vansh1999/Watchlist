85c74b4438b9c4a9d7ffcdb89b33fead561e1815
vansh1999 <vansh.bhardwaj1999@gmail.com>
2021-04-06 14:21:51 +0530

    building result cards

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -3,7 +3,10 @@ import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import Resultcard from "./Resultcard";
 const useStyles = makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   content: {
@@ -30,30 +33,69 @@ const Watchlist = () => {
 
   const [query, setQuery] = useState("");
 
+  const [results, setResults] = useState([]);
+
   const onChange = (e) => {
     e.preventDefault();
     setQuery(e.target.value);
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&query=${e.target.value}&page=1&include_adult=true
+    `)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+      });
   };
 
   return (
     <div>
       <Grid container justify="center" spacing={4}>
-        <Grid item xs={12} sm={6} md={6} lg={6} mt={6}>
+        <Grid item xs={2} sm={2} md={2} lg={2} mt={2}></Grid>
+        <Grid item xs={6} sm={6} md={6} lg={6} mt={6}>
           <TextField
-            id="filled-full-width"
-            style={{ margin: 8, marginTop: 30 }}
+            id="standard-basic"
+            style={{ margin: 8, marginTop: 90 }}
             placeholder="Search for a Movie"
             fullWidth
             margin="normal"
             InputLabelProps={{
               shrink: true,
             }}
-            variant="filled"
+            // variant="filled"
             value={query}
             onChange={onChange}
           />
-          <div id="text">{query}</div>
         </Grid>
+
+        <Grid item xs={2} sm={2} md={2} lg={2} mt={2}>
+          <label htmlFor="contained-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              style={{ margin: 8, marginTop: 90 }}
+            >
+              Search
+            </Button>
+          </label>
+        </Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} mt={2}></Grid>
+        {/* <div id="text">{query}</div> */}
+      </Grid>
+
+      <Grid container justify="center">
+        {results.length > 0 && (
+          <Grid item xs={2} sm={2} md={2} lg={2}>
+            {results.map((movie) => (
+              <Resultcard />
+            ))}
+          </Grid>
+        )}
       </Grid>
     </div>
   );
